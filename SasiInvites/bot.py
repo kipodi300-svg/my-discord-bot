4d69a1557589df3d4fa69d662a02ffe84037d9bb
kipodi300-svg <kipodi300@gmail.com>
2026-08-24 22:00:57 +0300

    Update bot.py

--- SasiInvites/bot.py
+++ SasiInvites/bot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 from discord.ext import commands
 
@@ -184,5 +185,5 @@ async def leaderboard_cmd(ctx):
     except discord.Forbidden:
         await ctx.send("❌ לבוט אין הרשאות לצפות בהזמנות בשרת זה.")
 
-TOKEN = "MTU0MTQ3NzEzOTQ2NjAyNzAyOA.GMLusH.3eoE0rZ-1Kely-YfHcePxNnpwbprkccc5DhNUI"
-bot.run(TOKEN)
+TOKEN = "MTU0MTQ3NzEzOTQ2NjAyNzAyOA.Gnj4fP.67fLrrlNkMXYkL4pfQRO9p2mfRxWMmii72H5ns"
+bot.run(os.getenv('MTU0MTQ3NzEzOTQ2NjAyNzAyOA.Gnj4fP.67fLrrlNkMXYkL4pfQRO9p2mfRxWMmii72H5ns'))
